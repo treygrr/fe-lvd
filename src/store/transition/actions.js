abe952818071dee39034e9720b8ff24e5c214fde
treygrr <phlasc@gmail.com>
2021-06-01 09:26:43 -0400

    Add actions for getting blog data

--- src/store/transition/actions.js
+++ src/store/transition/actions.js
@@ -3,12 +3,12 @@ import api from '../../boot/api'
 
 export function CHANGE_PAGE (commit, param) {
     if (this.$router.history.current.path === param.path) return;
-    const pushPath = param.path;
     this.commit('transition/startTransition')
     setTimeout(() => {
         this.commit('transition/stopTransition')
         if (this.$router.history.current.path === param.path) return;
-        this.$router.push({ path: pushPath })
+        console.log({ path: param.path, query: param.blogid })
+        this.$router.push({ path: param.path, query: {blogid: param.blogid} })
     }, 1000);
 }
 
@@ -16,5 +16,19 @@ export function GET_BLOG_POSTS ({commit}) {
     axios.get(`${api.url}blog`)
         .then(response => {
         commit('SET_BLOG_POSTS', response.data)
+    })
+}
+
+export function GET_BLOG_POSTS_PAGE ({commit}, param) {
+    axios.get(param.url)
+        .then(response => {
+        commit('SET_BLOG_POSTS', response.data)
+    })
+}
+
+export function GET_SINGLE_BLOG_POST ({ commit }, param) {
+    axios.get(`${api.url}blog/${param.blogid}`)
+        .then(response => {
+        commit('SET_BLOG_POST', response.data)
     })
 }
